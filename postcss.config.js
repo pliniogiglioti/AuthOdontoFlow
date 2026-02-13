cf62f83bf21803b5f8b15fe92f896dfbbc1e4af2
pliniogiglioti <plinio.giglioti@gmail.com>
2026-02-13 02:52:30 -0300

    Add postcss.config.js with Tailwind and Autoprefixer configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// Autoprefixer configuration
+module.exports = {
   plugins: {
-    tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
